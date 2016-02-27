003877d97ba825d7074532a3616e13c89a391bce
JOSE RINCON <JOSE RINCON@LUIS>
2016-02-27 03:28:30 -0500

    New commit

---
+++ src/diagrama3/Diagrama3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diagrama3;
+
+/**
+ *
+ * @author José Luis Rincón y Diana Marcela Molina
+ * @version 1.0
+ * since 02/24/2016
+ */
+public class Diagrama3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Primera Función en Main");
+        func1();
+        func2();
+        System.out.println("Se termina Main");
+    }
+    static void func1(){
+        System.out.println("Segunda Función");
+    }
+    static void func2(){
+        System.out.println("Tercera Función");
+    }
+}
